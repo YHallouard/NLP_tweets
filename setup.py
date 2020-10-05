d584c8fd2a582810a9f805cb712616eed5b9fbe6
yann.hallouard <yann.hal@gmail.com>
2020-10-05 11:38:14 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+required = ['matplotlib',
+            'numpy',
+            'pandas',
+            'notebook',
+            'jupyter'
+            ]
+
+__version__ = 'init'
+exec(open('nlp_tweets/version.py').read())
+
+setup(
+    name='nlp_tweets',
+    packages=find_packages(),
+    version=__version__,
+    description="NLP model for tweets classification",
+    author='Yann HALLOUARD',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    setup_requires=['pytest-runner', 'wheel', 'flake8'],
+    tests_require=['pytest', 'pytest-cov', 'treon'],
+    install_requires=required,
+    license='MIT\
+    ',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
